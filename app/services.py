2e79bde5c38dbbc4389efdcb066bf3cf5d56bb2a
Rohit Patil <patil.rohit048@gmail.com>
2023-02-23 18:28:25 +0530

    Update services.py

--- app/services.py
+++ app/services.py
@@ -6,8 +6,16 @@ BASE_DIR = Path(__file__).resolve().parent
 datafolder = os.path.join(BASE_DIR, "data")
 datasource = os.path.join(datafolder, "users.json")
 
-
+def check_dataset_exists():
+    if not os.path.exists(datafolder):
+        os.mkdir(datafolder)
+    if not os.path.exists(datasource):
+        with open(datasource, "w") as f:
+            f.write("")
+            
+            
 def read_usersdata():
+    check_dataset_exists()
     with open(datasource, "r") as f:
         content = f.read()
         if content == "":
